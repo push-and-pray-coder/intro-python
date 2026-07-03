3fbb482a5a8869cd2b85a8933bf4b506fe12df26
push-and-pray-coder <Titan.Chen@sdgku.edu>
2026-07-02 19:36:43 -0700

    class 1

--- dog_age_converter.py
+++ dog_age_converter.py
@@ -1,5 +1,5 @@
 #get the dogs age via input and convert it to a integer in human_age
-human_age = int(input("Enter dogs age: "))
+human_age = int(input("Enter dog's age: "))
 #calculate the dogs age and store it in dog_age
 dog_age = 7 * human_age
 
--- operators.py
+++ operators.py
@@ -22,4 +22,30 @@ print (res)
 # = means assignment
 # +=, -=, *=, /= shortcuts
 
-x=5
+x = 5
+x += 5
+x -= 3
+x *= 3
+x /= 2
+print(x)
+
+# COMPARISON OPERATORS - used to compare two values
+# == (equal to), !=(not equal), <=(less than) >=(greater than)
+
+# LOGICAL OPERATORS - used to combine conditional statemants (booleans T/F)
+# and -> both must be True
+# or -> at least one must be True
+# not -> flips True to False (vice versa)
+
+x = 3
+y = 10
+z = 3
+
+print(x == y and y == z)  # False, because both conditions are NOT true
+print(x == y or y == z)   # True, because one condition is true
+print(not x == y)         # True, because x != y
+
+x = [1, 2, 3, 4, 5]
+
+print (4 in x)
+print (9 not in x)
